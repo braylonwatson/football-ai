f18d2904defeb6f37f8f1495187d5f4d52cfa01d
Braylon Watson <braylon5watson@gmail.com>
2026-03-22 01:59:19 -0500

    connect frontend to deployed backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-const API = "http://127.0.0.1:8000";
+const API = process.env.REACT_APP_API_URL;
 
 function App() {
   const [teamsSet, setTeamsSet] = useState(false);
